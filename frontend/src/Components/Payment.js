e2cd1b28606216d27987f9125b8221a0fc89c1b4
sonyaaat <sofiatkach2004@gmail.com>
2022-12-18 14:17:32 +0200

    Payment fetch

--- frontend/src/Components/Payment.js
+++ frontend/src/Components/Payment.js
@@ -1,13 +1,18 @@
 import axios from "axios";
+const config={
+    headers: {"Access-Control-Allow-Origin": "*"}
+    // {withCredentials: true}
+}
 const fetchPayment=id=>{
     axios.post('http://127.0.0.1:8080/create/payment', {
     subscription_type_id:id
-  },{withCredentials: true})
+  },{withCredentials: true},config)
   .then(function (response) {
     console.log(response);
   }
-  .catch(function (error) {
-    console.log(error.toJSON());
-  }))
+//   .catch(function (error) {
+//     console.log(error.toJSON());
+//   })
+)
 }
 export default fetchPayment
